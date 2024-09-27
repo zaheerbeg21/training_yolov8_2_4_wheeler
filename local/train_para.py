40d843296d2517df01d9b8882edae9951c9b1a17
zaheerbeg21 <zaheerbeg21@gmail.com>
2024-09-27 16:07:29 +0530

    updated train_para

--- local/train_para.py
+++ local/train_para.py
@@ -4,24 +4,33 @@ from ultralytics import YOLO
 
 # Load a model
 model = YOLO('yolov8n.yaml')  # build a new model from YAML
+
 ## load a pretrained model (recommended for training)
 model = YOLO('/home/training_yolov8_2_4_wheeler/runs/detect/train/weights/last.pt')   # always use absolute path
 
 conf_path = '/home/training_yolov8_2_4_wheeler/local/config.yaml'   # always use absolute path
+
 number_of_epochs = 300
 initial_learning_rate= 0.01  # initial learning rate
 final_learning_rate = 0.01 # final learning rate (lr0 * lrf))  
 patience = 100   #If you want the model to train for longer, you can increase the patience
+iterations=300
+optimizer="AdamW"
+image_size = 640
+batch = 16
 
 ## Use the model
-results = model.train(data=conf_path, 
-                        epochs=number_of_epochs, 
-                        project='yolov8_license_plate_detection',
-                        name='model_1', # creates a new folder for each run 
+results = model.train(data = conf_path, 
+                        epochs = number_of_epochs, 
+                        project ='yolov8_license_plate_detection',
+                        name ='model_1', # creates a new folder for each run 
                         lr0 = initial_learning_rate,  
                         lrf = final_learning_rate, 
-                        patience=patience)  
-
+                        patience = patience,
+                        iterations = iterations,
+                        optimizer = optimizer,
+                        imgsz = image_size, 
+                        batch = batch)
 
 ## Resume training
 # results = model.train(resume=True)
